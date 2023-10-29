04ee94ee1089b3f740d6373e6c9c81c65b2e1f11
NitroRonak <ronaks0988@gmail.com>
2023-10-30 02:18:45 +0530

    update listing api complete

--- api/routes/listingRoute.js
+++ api/routes/listingRoute.js
@@ -1,8 +1,9 @@
 import express from 'express';
-import { createListing, deleteListing } from '../controllers/listingController.js';
+import { createListing, deleteListing, updateListing } from '../controllers/listingController.js';
 import { verifyUser } from '../utils/verifyUser.js';
 const router=express.Router();
 
 router.post('/create',verifyUser, createListing);
 router.delete('/delete/:id',verifyUser,deleteListing);
+router.put('/update/:id',verifyUser,updateListing);
 export default router;
